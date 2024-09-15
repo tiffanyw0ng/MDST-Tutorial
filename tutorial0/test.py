d2e5a8b1a5fa58696ae4a81cd9c7b594b09da96f
Tiffany Rosalyn Wong <tiffany@Tiffanys-Laptop.local>
2024-09-15 19:17:08 -0400

    Final commit

--- tutorial0/test.py
+++ tutorial0/test.py
@@ -3,8 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt  # Correct module from matplotlib
 
 # Load the CSV file
-data = pd.read_csv('tutorial0/states_edu.csv')
+data = pd.read_csv('../data/states_edu.csv')
 
 # Optional: Display the first few rows
 print(data.head())
+plt.scatter(df['STATE_REVENUE_PER_STUDENT'], df['AVG_MATH_8_SCORE'])
+plt.xlabel('STATE_REVENUE_PER_STUDENT')
+plt.ylabel('AVG_MATH_8_SCORE')
+plt.title('Relationship between STATE_REVENUE_PER_STUDENT and AVG_MATH_8_SCORE')
+plt.show()
 
